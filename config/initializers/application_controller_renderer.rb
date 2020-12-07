4d6b0e02fbb5f64671c577d652378898b9e3d10e
Willian Gustavo Veiga <beberveiga@gmail.com>
2020-12-07 19:45:32 -0300

    Make string literals frozen and remove unused default Rails comments

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+# frozen_string_literal: true
